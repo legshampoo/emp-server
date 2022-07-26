a586dfcf1257e40696aecdc1e9b692a729d0ad6b
Dan Baker <daniel.c.baker@gmail.com>
2022-07-26 13:26:49 +0700

    env vars

--- README.md
+++ README.md
@@ -1,7 +1,4 @@
 
 
-trying to update...
-
-
 heroku create empmoney-server
 https://empmoney-server.herokuapp.com/ | https://git.heroku.com/empmoney-server.git
--- index.js
+++ index.js
@@ -12,5 +12,16 @@ app.get('/api/twap-history', getTwapHistory);
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`EMP server is running....`)
+  const envVars = {
+    port: process.env.PORT,
+    mongodbUri: process.env.MONGODB_URI,
+    oracle: process.env.ORACLE,
+    provider: process.env.PROVIDER,
+    emp_address: process.env.EMP_ADDRESS,
+    database: process.env.DATABASE,
+    twapFetchInterval: process.env.TWAP_FETCH_INTERVAL
+  }
+  
+  console.log(envVars);
   init();
 })
